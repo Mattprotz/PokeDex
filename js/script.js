83cac607e9ad5e548982766eb267ebbe7be9a5ef
Matthew Proteau <matthew.proteau@gmail.com>
2023-11-28 11:03:08 -0800

    update js
    added more pokemon, created eventListener that logs pokemon details

--- js/script.js
+++ js/script.js
@@ -23,34 +23,80 @@ let Beedrill = {
     type: ['bug' , 'poison']
 }
 
+let Jigglypuff = {
+    name : 'Jigglypuff',
+    height : 1.5,
+    type : ['normal', 'fairy']
+}
+
+let Meowth = {
+    name : 'Meowth',
+    height : 1.4,
+    type : ['normal']
+}
+let Diglett = {
+    name : 'Diglett',
+    height : 1.8,
+    type : ['ground']
+}
+let Croagunk = {
+    name : 'Croagunk',
+    height : 2.4,
+    type : ['poison', 'fighting']
+}
+
 let pokemonList = [ 
     Bulbasor, 
     Gastly,
     Butterfree, 
-    Beedrill
+    Beedrill,
+    Jigglypuff,
+    Meowth,
+    Diglett,
+    Croagunk
 ];
 
 let pokemonRepository = (function(){
     let pokemonList=[]
-
+    let addListItem;
+    
     return{
         add: function(pokemon){
             pokemonList.push(pokemon);
         },
         getAll:function(){
-            return pokemonList;
+            return repository.pokemonList;
+        },
+        addListItem:function(pokemon){
+            let listItem = document.createElement('li');
+            let button = document.createElement('button');
+            button.innerText = pokemon.name;
+            button.classList.add('button-class');
+            uListItem.appendChild(listItem);
+            listItem.appendChild(button);
+
+            button.addEventListener('click' , function(showDetails){
+                console.log(pokemon)
+            });
+        },
+        showDetails:function(pokemon){
+            
         }
-    }
+    };
+
+    return repository;
+
 })();
 
 pokemonRepository.add({
     name: 'Pikachu',
     height: 0.7,
-    type : ['electricity']
+    type : ['electricity' , 'swag']
 });
 
+let uListItem = document.querySelector('ul');
 
 pokemonList.forEach(function(pokemon){
-    document.querySelector('ul')
-    console.log('hello')
+    pokemonRepository.addListItem(pokemon);
 });
+
